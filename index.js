6e316106d17f1aafa0937f93c36c6ba273193a66
DisaniaS <108000102+DisaniaS@users.noreply.github.com>
2023-11-25 18:36:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res)=>{
-    res.send('Hello world!')
+    res.send('Hello world! this is my homework')
 });
 
 app.listen(4444, (err) => {
